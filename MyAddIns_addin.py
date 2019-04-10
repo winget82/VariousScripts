4df7e65faaac3a2e32b776be638b53a8203cb23f
Nate <40738033+winget82@users.noreply.github.com>
2019-04-09 21:13:40 -0500

    Add files via upload
    Simple script to return a formatted text to the clipboard of a couple fields in a feature class

--- MyAddIns_addin.py
+++ MyAddIns_addin.py
@@ -10,11 +10,16 @@ class ButtonClass1(object):
         self.checked = False
     # Implementation of OnClick method of Button's class
     def onClick(self):
-
-        with arcpy.da.SearchCursor('', ['CROSS_NBR', 'ROAD_NAME']) as cursor:
+        # Get the current map document and the first data frame
+        #mxd = arcpy.mapping.MapDocument('current')
+        #df = arcpy.mapping.ListDataFrames(mxd)[0]
+        # Call the zoomToSelectedFeatures() method of the data frame class
+        #df.zoomToSelectedFeatures()
+        with arcpy.da.SearchCursor('FEATURE_CLASS', ['CROSS_NBR', 'ROAD_NAME']) as cursor:
             for row in cursor:
                 x = row[0]
                 y = row[1]
                 roadxng = str(x) + ', ' + str(y)
                 rdxing = roadxng.upper()
                 pyperclip.copy(str(rdxing.strip())) 
+# This formats two fields and copies them to the clipboard for manual pasting
